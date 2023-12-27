f5a7e665cb9933f9a350ef5bc345926e60ce1c63
Paul Sisson <paulsissonsemail@gmail.com>
2023-12-27 13:56:22 -0600

    All done.

--- contexts.py
+++ contexts.py
@@ -51,16 +51,37 @@ class PoemFiles:
   
 
 # now let's use that import to do this same thing but prettier
+# -- note: to handle errors using this setup we can simply do nothing and an error will be thrown
+# -- OR: we can add an "except" block between try and finally with our error logic
 @contextmanager
 def pretty_poem_files(file, mode):
   open_file = open(file, mode)
   try:
     print('Pretty file opening')
+    print('------- A sonnet by Paul: -------')
     yield open_file
+  except AttributeError as err:
+    print(f'ERr0r.... 3Rr0R... uhh Two roads diverged in a yellow wood an..a. aa=hosdigha...{err}')
   finally:
+    print('---------------------------------')
     print('Closing pretty file')
     open_file.close()
     
     
 with pretty_poem_files('poem.txt', 'r') as ppf:
   print(ppf.read())
+# Pretty file opening
+# ------- A sonnet by Paul: -------
+# Roses are red
+# Violets are blue
+# Sometimes it don't be like that
+# But sometimes it do
+# ---------------------------------
+# Closing pretty file
+# with pretty_poem_files('poem.txt', 'r') as ppf2:
+#   print(ppf2.errormaker())
+# Pretty file opening
+# ------- A sonnet by Paul: -------
+# ERr0r.... 3Rr0R... uhh Two roads diverged in a yellow wood an..a. aa=hosdigha...'_io.TextIOWrapper' object has no attribute 'errormaker'
+# ---------------------------------
+# Closing pretty file
